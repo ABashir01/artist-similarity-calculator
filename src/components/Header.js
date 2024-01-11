d6c817a3fb890153644fdd6bd54b600832dbf899
ABashir01 <ahadbashirblast@gmail.com>
2024-01-11 12:52:36 -0600

    Added noopener and noreferrer to links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ function Header() {
         <div className="header-div">
             <div className='title-div'><b>Artist Similarity Calculator</b> by Ahad Bashir</div>
             <div className='links-div'>
-                <a href='https://ahad-bashir-portfolio.netlify.app/' target='_blank'>Portfolio</a>
-                <a href='https://github.com/ABashir01/artist-similarity-calculator' target='_blank'>Github</a>
+                <a href='https://ahad-bashir-portfolio.netlify.app/' target='_blank' rel="noopener noreferrer">Portfolio</a>
+                <a href='https://github.com/ABashir01/artist-similarity-calculator' target='_blank' rel="noopener noreferrer">Github</a>
             </div>
         </div>
     );
